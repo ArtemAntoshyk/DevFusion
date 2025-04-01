a57d01d2d865c1d953c7170fec33e049b001de65
artem <yaroslav.gorbatyuk.2010@gmail.com>
2025-04-01 22:47:48 +0300

    Fourth test commit

--- src/main/java/devtitans/antoshchuk/devfusion2025backend/controllers/AuthController.java
+++ src/main/java/devtitans/antoshchuk/devfusion2025backend/controllers/AuthController.java
@@ -2,11 +2,8 @@ package devtitans.antoshchuk.devfusion2025backend.controllers;
 
 import devtitans.antoshchuk.devfusion2025backend.dto.request.UserLoginRequestDTO;
 import devtitans.antoshchuk.devfusion2025backend.dto.request.UserRegisterRequestDTO;
-import devtitans.antoshchuk.devfusion2025backend.dto.request.UserSeekerRegisterRequestDTO;
-import devtitans.antoshchuk.devfusion2025backend.models.user.UserAccount;
 import devtitans.antoshchuk.devfusion2025backend.security.util.jwt.JwtTokenProvider;
 import devtitans.antoshchuk.devfusion2025backend.services.AuthService;
-import devtitans.antoshchuk.devfusion2025backend.services.CustomUserDetailsService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +11,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
